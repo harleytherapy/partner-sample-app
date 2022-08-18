a82005220df4ada4a884079415815343afbf6c5f
Iain Donaldson <iain@sotp.xyz>
2022-08-18 16:35:00 +0100

    fix: updated sample to work with new stricter error checking

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const host = process.env.HOST
 const path = '/clients'
 const method = 'POST'
 const requestId = uuid()
-const isoDate = (new Date).toISOString()
+const isoDate = new Date().toISOString()
 const payload = `${method} ${path} ${requestId} ${isoDate}`
 const authString = encHex.stringify(hmacSHA256(payload, authSecret))
 const body = {
@@ -22,39 +22,38 @@ const body = {
   phone_number: '+447999999999',
   first_name: 'First',
   last_name: 'Last',
-  date_of_birth: '1986-10-04',
-  gender: 'male'
+  date_of_birth: '04/10/2001',
+  gender: 'male',
 }
 
 const checkStatus = (res) => {
-    if (res.ok) { // res.status >= 200 && res.status < 300
-        return res;
-    } else {
-        throw new Error(`${res.status} - ${res.statusText}`);
-    }
+  if (res.ok) {
+    return res
+  } else {
+    res.json().then((error) => {
+      throw new Error(JSON.stringify(error, null, 2))
+    })
+  }
 }
 
 fetch(`${host}${path}`, {
   method: method,
   body: JSON.stringify(body),
   headers: {
-    'Authorization': `${authId}:${authString}`,
-    'Date': isoDate,
+    Authorization: `${authId}:${authString}`,
+    Date: isoDate,
     'X-HT-Request-id': requestId,
     'Content-type': 'application/json',
-    'Accept': 'application/vnd.harleytherapyplatform.v1+json'
-  }
+    Accept: 'application/vnd.harleytherapyplatform.v1+json',
+    'User-Agent': 'Awesome-App',
+  },
 })
   .then(checkStatus)
   .then((response) => {
-    if (response.status == 404) {
-      console.log(response.statusText)
-    } else {
-      return response.json()
-    }
+    if (response) return response.json()
   })
   .then((data) => {
-    console.log(data);
+    if (data) console.log(data)
   })
   .catch((error) => {
     console.log(error)
